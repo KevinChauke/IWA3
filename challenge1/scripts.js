d9f18bf80953697de4ab0408d1590ecef3a10149
KevinChauke <s.chauke@lab.clicklearning.org>
2023-05-05 08:44:04 +0200

    finishing challenge 1

---
+++ challenge1/scripts.js
@@ -0,0 +1,8 @@
+import {company} from './configuration.js'
+import {year} from './configuration.js' 
+
+
+//console.log(company, year)
+
+const message = '© ' + company + ' (' + year + ')'
+document.querySelector('footer').innerText = message
